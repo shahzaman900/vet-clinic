148a9c124db457dab20e2e9cbc099bf87689403f
sheezu900 <shahzamanlive2@gmail.com>
2023-09-19 11:47:42 +0500

    insert data in animal table

--- data.sql
+++ data.sql
@@ -1,5 +1,8 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES
+  ( 1, 'Agumon', '03-02-2020', 0, true, 10.23),
+  ( 2, 'Gabumon', '15-11-2020', 0, true, 10.23),
+  ( 3, 'Pikachu', '07-01-2020', 0, true, 10.23),
+  ( 4, 'Devimon', '12-05-2020', 0, true, 10.23);
